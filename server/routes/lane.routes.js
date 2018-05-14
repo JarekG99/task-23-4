fc9d4596e29b3ae89739904657cf347abfef9ae6
Jarek Gajdulewicz <j.gajdulewicz@icloud.com>
2018-05-14 22:05:02 +0200

    Add server side

---
+++ server/routes/lane.routes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import * as LaneController from '../controllers/lane.controller';
+
+const router = new Router
+
+// Get all Lanes
+router.route('/lanes').get(LaneController.getLanes);
+
+// Add a new Lane
+router.route('/lanes').post(LaneController.addLane);
+
+// Delete a lane by laneId
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
+
+// Change lane name by laneId
+router.route('/lanes/:laneId').get(LaneController.changeLaneName);
+
+
+export default router;
